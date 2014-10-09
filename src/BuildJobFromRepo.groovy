8d1fd30e901cf72afb81c053f6cd2e2ed1cc9a3d
Roman Stoffel <roman.stoffel@gamlor.info>
2014-10-09 18:45:59 +0900

    Working changes

--- src/BuildJobFromRepo.groovy
+++ src/BuildJobFromRepo.groovy
@@ -1,3 +1,5 @@
+import groovy.json.JsonSlurper
+import groovy.transform.Immutable
 @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.1' )
 import groovyx.net.http.HTTPBuilder
 import groovyx.net.http.ContentType
@@ -17,19 +19,48 @@ listOfProjects.each { item ->
 def newJobs = listOfProjects.collect { item ->
     buildProjectXml(item,templateXml)
 }
+
+
 def buildProjectXml(File project, templateXml){
     def configXml = new XmlParser().parseText( XmlUtil.serialize( templateXml ) )
 
     def readProjectInfo = readInfo(project)
+
+    templateXml.project.disabled = "false"
+
+    def gitConfig = templateXml.depthFirst().find {
+        println(it.name)
+        it.name() == "hudson.plugins.git.UserRemoteConfig"
+    }
+    gitConfig.url = env['GIT_URL']
+
     return "hello"
 }
 
 def readInfo(File project){
     def versionTag = "head"
-    def versionFile = new File(project,"version.jenkins.txt")
+    def versionFile = new File(project,"autobuild.version.txt")
     if(versionFile.exists()){
         versionTag = versionFile.getText("UTF-8")
     }
 
-    def buildInfo = ""
+    def json = new JsonSlurper()
+
+    def platform = "32-bit"
+    def email = "roman@spoon.net"
+
+    def buildInfoFile = new File(project,"autobuild.build-info.txt")
+    if(buildInfoFile.exists()){
+        def buildInfo =json.parse(buildInfo)
+        platform = buildInfo.platform ?:platform
+        email = buildInfo.email ?:email
+    }
+    new BuildInfo(email:email, platform: platform,  version:versionTag)
+}
+
+@Immutable class BuildInfo {
+    String email
+    String plattform
+    String version
+
 }
--- src/InitialGrabJobCreator.groovy
+++ src/InitialGrabJobCreator.groovy
@@ -6,11 +6,10 @@ import groovy.xml.XmlUtil
 
 
 
-
 def env = System.getenv()
 
-if(null==env['GIT_REPO']){
-    throw new EnvironmentVariableMissing("GIT_REPO")
+if(null==env['GIT_URL']){
+    throw new EnvironmentVariableMissing("GIT_URL")
 }
 if(null==env['JENKINS_URL']){
     throw new EnvironmentVariableMissing("JENKINS_URL")
@@ -24,7 +23,7 @@ def jenkinsApi = new HTTPBuilder(env['JENKINS_URL'])
 def templateXml = jenkinsApi.get(path: '/job/Template-Monitor-Git-And-Create-Job/config.xml',
         contentType : ContentType.XML)
 
-
+templateXml.project.disabled = "false"
 
 
 def gitConfig = templateXml.depthFirst().find {
@@ -34,21 +33,21 @@ def gitConfig = templateXml.depthFirst().find {
 gitConfig.url = env['GIT_REPO']
 
 def user = "spoon-jenkins-user"
-def mainRepoName = new URI(env['GIT_REPO']).getPath().split("/").last()
+def mainRepoName = new URI(env['GIT_URL']).getPath().split("/").last().split("\\.").first()
 def jobname = "z-auto-$user-$mainRepoName-job-control"
+
 
 def bodyTxt = XmlUtil.serialize(templateXml)
 jenkinsApi.request( Method.POST, ContentType.XML ) {
     uri.path = '/createItem'
-    uri.query = [name:'AA-Test-2']
+    uri.query = [name:jobname]
     body = bodyTxt
 
 }
 
-jenkinsApi.request(Method.POST, ContentType.XML) {
-    uri.path = '/job/'
-    uri.query = [name:'AA-Test-2']
-    body = bodyTxt
+jenkinsApi.request(Method.POST, ContentType.TEXT) {
+    uri.path = "/job/$jobname/build"
+    body = ""
 
 }
 
