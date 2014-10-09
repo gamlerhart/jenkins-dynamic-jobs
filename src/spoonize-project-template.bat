251ed5c084ef4656a15dba3a7f20f808686ee1ef
Roman Stoffel <roman.stoffel@gamlor.info>
2014-10-09 23:11:38 +0900

    Ensure we logut first.

--- src/spoonize-project-template.bat
+++ src/spoonize-project-template.bat
@@ -10,6 +10,7 @@ if NOT DEFINED VERSION (
 	echo "VERSION is missing"
 	exit 1
 )
+spoon logout
 spoon login %SPOON_USER% %SPOON_PWD%
 spoon config --hub=http://dev-stage.spoon.net
 spoon build %SPOON_BUILD_FLAVOR% --overwrite --name=%SPOON_NAMESPACE%/%SPOON_REPO%:%VERSION% %SPOONFILE%
--- src/test-project-template.bat
+++ src/test-project-template.bat
@@ -2,6 +2,9 @@ set SPOON_NAMESPACE={namespace}
 set SPOON_REPO={repo-name}
 set VERSION={version}
 IF EXIST test (
+  spoon logout
+  spoon login %SPOON_USER% %SPOON_PWD%
+  spoon config --hub=http://dev-stage.spoon.net
   echo "Rough test before publishing"
   cd test/
 
@@ -12,11 +15,6 @@ IF EXIST test (
     echo "Failed test with %ERRORLEVEL%"
     exit %ERRORLEVEL%
   )
-  spoon push %SPOON_NAMESPACE%/%SPOON_REPO%:%VERSION%
-  rem This is to avoid filling the build machine will useless images
-  spoon rmi --all
 ) ELSE (
-  spoon push %SPOON_NAMESPACE%/%SPOON_REPO%:%VERSION%
-  rem This is to avoid filling the build machine will useless images
-  spoon rmi --all
+   echo "no tests. we're done"
 )
